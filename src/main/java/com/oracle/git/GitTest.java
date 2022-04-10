8d0f34b46ad44cd5c1d7fa39b33d7c5381cadb80
liqingfeng <1943757087@qq.com>
2022-04-10 21:30:39 +0800

    first commont

---
+++ src/main/java/com/oracle/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.oracle.git;
+
+/**
+ * @author shkstart
+ * @create 2022-04-10 21:29
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("第一句话。");
+    }
+}
