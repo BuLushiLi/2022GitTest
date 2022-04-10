60fa611e7a552459526659319aff113e1d24de29
liqingfeng <1943757087@qq.com>
2022-04-10 21:42:29 +0800

    second commont

---
+++ src/main/java/com/oracle/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.oracle.git;
+
+/**
+ * @author shkstart
+ * @create 2022-04-10 21:41
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("第一句话。");
+        System.out.println("第二句话。");
+        System.out.println("第三句话。");
+        System.out.println("第四句话。");
+    }
+}
